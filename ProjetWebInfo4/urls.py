fb5199ea4197e04ca20b03e3e5a2f14ac7b51b05
ptitFicus <benji.cavy@gmail.com>
2014-02-17 12:27:34 +0000

    Maj du fichier url de l'appli

--- ProjetWebInfo4/urls.py
+++ ProjetWebInfo4/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^absences/', include('absences.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
