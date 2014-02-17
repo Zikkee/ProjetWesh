9b3f1fb4aa1f0212756ad0f0ab51de6d77b31041
“Guillaume <guillaume.forgeon@gmail.com>
2014-02-17 21:44:28 +0100

    Avancement

--- absences/urls.py
+++ absences/urls.py
@@ -4,6 +4,6 @@ from absences import views
 
 urlpatterns = patterns('',
 	#url(r'^$', views.index, name='index'),
-	url(r'^cours/liste/$', views.listeCours, name='listeCours'),
+	url(r'^cours/liste/(\d{4})/(\d{2})/(\d{2})/$', views.CoursListView.as_view(), name='listeCours'),
 	url(r'^cours/(?P<cours_id>\d+)/$', views.consultationCours, name='consultationCours'),
 )
--- absences/views.py
+++ absences/views.py
@@ -1,10 +1,27 @@
+#-*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from absences.models import Cours 
 from django.http import HttpResponse
+from django.views.generic.list import ListView
 
-def listeCours (request):
-	listeCours = Cours.objects.all()
-	return render(request, 'absences/listeCours.html', {'listeCours':listeCours})
+from datetime import datetime
+
+class CoursListView(ListView):
+	model = Cours
+	context_object_name = 'listeCours' #permet de donner un nom autre que object_list pour la variable
+	template_name = 'absences/listeCours.html' #redéfinition du template, au lieu de cours_list.html
+
+	def get_queryset(self):
+		if self.args[0] and self.args[1] and self.args[2]:
+			dateAujourdhuiDebut = datetime(int(self.args[0]), int(self.args[1]), int(self.args[2]), 0, 0, 0)
+			dateAujourdhuiFin = datetime(int(self.args[0]), int(self.args[1]), int(self.args[2]), 23, 59, 59)
+		else:
+			dateAujourdhuiDebut = datetime.now().replace(hour=0, minute=0, second=0)
+			dateAujourdhuiFin = datetime.now().replace(hour=23, minute=59, second=59)
+
+		#permet de filtrer pour une date donnée (si pas de date, date d'aujourd'hui par défaut)
+		return Cours.objects.filter(dateDebut__gte = dateAujourdhuiDebut, dateFin__lte = dateAujourdhuiFin)
 
 def consultationCours(request, cours_id):
 	return HttpResponse("consultation du cours " + cours_id)
