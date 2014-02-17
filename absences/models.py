9f1b0bb8bebfdbc73b9628afa7e639c1c9a042e9
ptitFicus <benji.cavy@gmail.com>
2014-02-17 11:10:22 +0000

    Correction du modèle (il génère maintenant le SQL)

--- absences/models.py
+++ absences/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
+class Utilisateur(models.Model):
+	identifiant = models.CharField(max_length=200)
+	prenom = models.CharField(max_length=200)
+	mdp = models.CharField(max_length=200)
+	nom = models.CharField(max_length=200)
+	mail = models.CharField(max_length=200)
+
+class Secretaire(Utilisateur):
+	pass
+	
+class Enseignant(Utilisateur):
+	pass
+
+class Promotion(models.Model):
+	nom = models.CharField(max_length=200)
+	responsable = models.ForeignKey(Enseignant)
+
+class Matiere (models.Model):
+	intitule = models.CharField(max_length = 200)
+
+class Etudiant(Utilisateur):
+	promotion = models.ForeignKey(Promotion)
+	
+class Groupe(models.Model):
+	nom = models.CharField(max_length=200)
+	etudiants = models.ManyToManyField(Etudiant, related_name='membresGroupe+')
+
 class Cours (models.Model):
 	dateDebut = models.DateTimeField(auto_now = True, auto_now_add = False)
 	dateFin = models.DateTimeField(auto_now = True, auto_now_add = False)
@@ -7,9 +34,6 @@ class Cours (models.Model):
 	matiere = models.ForeignKey(Matiere)
 	donne_a = models.ForeignKey(Groupe)
 	donne_par = models.ManyToManyField(Enseignant, related_name = 'donne_par')
-
-class Matiere (models.Model):
-	intitule = models.CharField(max_length = 200)
 
 class Departement (models.Model):
 	nom = models.CharField(max_length = 200)
@@ -19,32 +43,10 @@ class Justificatif (models.Model):
 	genre = models.CharField(max_length = 200)
 	dateDebut = models.DateTimeField(auto_now = False, auto_now_add = False)
 	dateFin = models.DateTimeField(auto_now = False, auto_now_add = False)
-	matiere = models.ManyToManyField(Justificatif, related_name = 'vaut_pour_la_matiere')
-class Utilisateur(models.Model):
-	identifiant: models.CharField(max_length=200)
-	prenom: models.CharField(max_length=200)
-	mdp: models.CharField(max_length=200)
-	nom: models.CharField(max_length=200)
-	mail: models.CharField(max_length=200)
+	matiere = models.ManyToManyField(Matiere, related_name = 'vaut_pour_la_matiere')
 
-class Secretaire(Utilisateur):
-	
-class Etudiant(Utilisateur):
-	promotion: models.ForeignKey(Promotion)
-	
-	
-class Enseignant(Utilisateur):
-	
-class Promotion:
-	nom: models.CharField(max_length=200)
-	responsable: models.ForeignKey(Enseignant)
-
-class Groupe(models.Model):
-	nom: models.CharField(max_length=200)
-	etudiants: models.ManyToManyField(Etudiant, related_name='membresGroupe+')
-	
 class Absence:
-	etudiant: models.ForeignKey(Etudiant)
-	cours: models.ForeignKey(Cours)
-	justifie: models.BooleanField(default=False)
-	justificatif: models.ForeignKey(Justificatif)
+	etudiant = models.ForeignKey(Etudiant)
+	cours = models.ForeignKey(Cours)
+	justifie = models.BooleanField(default=False)
+	justificatif = models.ForeignKey(Justificatif)
