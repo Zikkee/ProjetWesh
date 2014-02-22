d2174703231d357ca1a665f8e230fefd8da2547a
Zikkee <guillaume.forgeon@gmail.com>
2014-02-22 22:28:54 +0100

    Ajout filtrage année + mois + jour + pagination

--- absences/urls.py
+++ absences/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import patterns, url
 from absences import views
 
 urlpatterns = patterns('',
-	#url(r'^$', views.absences.index, name='index'),
-	url(r'^cours/liste/(\d{4})/(\d{2})/(\d{2})/$', views.CoursListView.as_view(), name='listeCours'),
-	#url(r'^cours/liste/(\w+)/$', views.CoursListView.as_view(), name='listeCours'),
+	url(r'^$', views.index, name='index'),
+	url(r'^cours/liste/$', views.CoursListView.as_view(), name='listeCours'),
+	url(r'^cours/liste/(\d{4})/$', views.CoursListViewAnne.as_view(), name='listeCoursAnnee'),
+	url(r'^cours/liste/(\d{4})/(\d{2})/$', views.CoursListViewMois.as_view(), name='listeCoursMois'),
+	url(r'^cours/liste/(\d{4})/(\d{2})/(\d{2})/$', views.CoursListViewJour.as_view(), name='listeCoursJour'),
 	url(r'^cours/(?P<cours_id>\d+)/$', views.consultationCours, name='consultationCours'),
 )
--- absences/views.py
+++ absences/views.py
@@ -1,30 +1,48 @@
 #-*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from absences.models import Cours 
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+
+from absences.models import Cours 
+
+from datetime import datetime
+from calendar import monthrange
 
 def index(request):
 	return render(request, 'absences/index.html')
 
+# Affiche tous les cours de la bdd
 class CoursListView(ListView):
 	model = Cours
-	context_object_name = 'listeCours' #permet de donner un nom autre que object_list pour la variable
-	template_name = 'absences/listeCours.html' #redéfinition du template, au lieu de cours_list.html
+	context_object_name = 'listeCours'
+	template_name = 'absences/listeCours.html'
+	paginate_by = 1
 
+# Affiche tous les cours d'une année
+class CoursListViewAnne (CoursListView):
+	def get_queryset(self): 
+		dateDebut = datetime(int(self.args[0]), 1, 1, 0, 0, 0)
+		dateFin = datetime(int(self.args[0]), 12, 31, 23, 59, 59)
+
+		return Cours.objects.filter(dateDebut__gte = dateDebut, dateFin__lte = dateFin)
+
+# Affiche tous les cours d'un mois
+class CoursListViewMois (CoursListView):
 	def get_queryset(self):
-		if self.args[0]:
-			dateAujourdhuiDebut = datetime.now()
-			dateAujourdhuiFin = datetime.now()
-			# dateAujourdhuiDebut = datetime(int(self.args[0]), int(self.args[1]), int(self.args[2]), 0, 0, 0)
-			# dateAujourdhuiFin = datetime(int(self.args[0]), int(self.args[1]), int(self.args[2]), 23, 59, 59)
-		else:
-			dateAujourdhuiDebut = datetime.now().replace(hour=0, minute=0, second=0)
-			dateAujourdhuiFin = datetime.now().replace(hour=23, minute=59, second=59)
+		r = monthrange(int(self.args[0]), int(self.args[1])) # Pour connaître le nombre de jours dans le mois
+		nbJours = r[1]
 
-		#permet de filtrer pour une date donnée (si pas de date, date d'aujourd'hui par défaut)
-		return Cours.objects.filter(dateDebut__gte = dateAujourdhuiDebut, dateFin__lte = dateAujourdhuiFin)
+		dateDebut = datetime(int(self.args[0]), int(self.args[1]), 1, 0, 0, 0)
+		dateFin = datetime(int(self.args[0]), int(self.args[1]), nbJours, 23, 59, 59)
+		return Cours.objects.filter(dateDebut__gte = dateDebut, dateFin__lte = dateFin)
+
+# Affiche tous les cours d'un jour
+class CoursListViewJour (CoursListView):
+	def get_queryset(self):
+		dateDebut = datetime(int(self.args[0]), int(self.args[1]), int(self.args[2]), 0, 0, 0)
+		dateFin = datetime(int(self.args[0]), int(self.args[1]), int(self.args[2]), 23, 59, 59)
+		return Cours.objects.filter(dateDebut__gte = dateDebut, dateFin__lte = dateFin)
 
 def consultationCours(request, cours_id):
 	return HttpResponse("consultation du cours " + cours_id)
