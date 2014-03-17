46e9d331969820ff3555090c3b9300c419740128
ptitFicus <benji.cavy@gmail.com>
2014-03-17 22:41:26 +0000

    Création d'interface basique pour la vue des élèves

--- absences/urls.py
+++ absences/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
 
-from absences import listeEleve
+from absences import views
 
 urlpatterns = patterns('',
-    url(r'^$', listeEleve.listeEleve, name='listeEleve')
+    url(r'^eleves/$', views.listeEleve, name='listeEleve'),
+    url(r'^eleves/liste/(?P<idEleve>\d+)/$', views.infosEleve, name='infosEleve')
 )
-
--- absences/views.py
+++ absences/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from absences.models import Etudiant
 
-# Create your views here.
+def listeEleve(request):
+    listeEleve = Etudiant.objects.all()
+    context = {'listeEleve': listeEleve}
+    return render(request, 'absences/listeEleve.html', context)
+    #output = ', '.join([e.prenom + " " +e.nom for e in listeEleve])
+    #return HttpResponse(output)
+    
+def infosEleve(request, idEleve):
+    try:
+        eleve = Etudiant.objects.get(id=idEleve)
+    except Etudiant.DoesNotExist:
+        raise Http404
+    return render(request, 'absences/infosEleve.html', {'eleve': eleve})
